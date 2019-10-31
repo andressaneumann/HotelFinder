9d7ab07f973d47bcfde0ec4aaf0d5caa66e7c46f
Andressa Neumann <neumm.andressa@gmail.com>
2019-10-31 19:44:28 -0300

    working on list of days

--- Test/Logic.cs
+++ Test/Logic.cs
@@ -8,12 +8,42 @@ namespace Test
 {
     public class Logic
     {
-        public string CalculateBestHotel(string userInput)
-        {
-            string[] splittedUserInputs = SplitUserInput(userInput);
 
-            foreach (var word in splittedUserInputs)
-                Console.WriteLine(word);
+        float LakewoodWeekTaxRegularCustomer = 110;
+        float LakewoodWeekTaxFidelityCustomer = 80;
+        float LakewoodWeekendTaxRegularCustomer = 90;
+        float LakewoodWeekendTaxFidelityCustomer = 80;
+
+
+        float BridgewoodWeekTaxRegularCustomer = 160;
+        float BridgewoodWeekTaxFidelityCustomer = 110;
+        float BridgewoodWeekendTaxRegularCustomer = 60;
+        float BridgewoodWeekendTaxFidelityCustomer = 50;
+
+        float RidgewoodWeekTaxRegularCustomer = 220;
+        float RidgewoodWeekTaxFidelityCustomer = 100;
+        float RidgewoodWeekendTaxRegularCustomer = 150;
+        float RidgewoodWeekendTaxFidelityCustomer = 40;
+
+        string[] dayOfTheWeek;
+
+        public string CalculateBestHotel(string userInput)
+        {        
+
+            string[] splittedUserInputs = SplitUserInput(userInput);
+            string CustomerType = splittedUserInputs[0];
+
+            var numberOfEntriesOnUserInput = splittedUserInputs.Length;
+            for(int i = 1; i <= numberOfEntriesOnUserInput; i++)
+            {
+                if (i % 2 == 0)
+                    dayOfTheWeek[d] = splittedUserInputs[i];
+
+                d++;
+            }
+
+            foreach (var day in dayOfTheWeek)
+                Console.WriteLine(day);
 
             return "a";
         }
